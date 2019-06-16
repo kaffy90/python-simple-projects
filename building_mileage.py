b4b1bbefc0bee85b1ba6891fb107c03655ab7e36
kaffy90 <adeoye.a.kafayat@gmail.com>
2019-06-16 08:08:49 +0100

    add a line

--- building_mileage.py
+++ building_mileage.py
@@ -2,3 +2,4 @@
 kms = int(input("Enter your distance: " ""))
 miles= (kms/25)  # formula to calculate distance or convert degree
 print(f"Great, your miles value is {round(miles,2)}")
+print("Great Speed")
